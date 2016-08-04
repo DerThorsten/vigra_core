c1e54a8f5356b3c736e5e4f1edae2f741ebda353
Ullrich Koethe <ullrich.koethe@iwr.uni-heidelberg.de>
2016-08-04 18:44:07 +0200

    added windows.h

---
+++ include/vigra2/windows.h
@@ -0,0 +1,64 @@
+/************************************************************************/
+/*                                                                      */
+/*               Copyright 2014-2016 by Ullrich Koethe                  */
+/*                                                                      */
+/*    This file is part of the VIGRA2 computer vision library.          */
+/*    The VIGRA2 Website is                                             */
+/*        http://ukoethe.github.io/vigra2                               */
+/*    Please direct questions, bug reports, and contributions to        */
+/*        ullrich.koethe@iwr.uni-heidelberg.de    or                    */
+/*        vigra@informatik.uni-hamburg.de                               */
+/*                                                                      */
+/*    Permission is hereby granted, free of charge, to any person       */
+/*    obtaining a copy of this software and associated documentation    */
+/*    files (the "Software"), to deal in the Software without           */
+/*    restriction, including without limitation the rights to use,      */
+/*    copy, modify, merge, publish, distribute, sublicense, and/or      */
+/*    sell copies of the Software, and to permit persons to whom the    */
+/*    Software is furnished to do so, subject to the following          */
+/*    conditions:                                                       */
+/*                                                                      */
+/*    The above copyright notice and this permission notice shall be    */
+/*    included in all copies or substantial portions of the             */
+/*    Software.                                                         */
+/*                                                                      */
+/*    THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND    */
+/*    EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES   */
+/*    OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND          */
+/*    NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT       */
+/*    HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,      */
+/*    WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING      */
+/*    FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR     */
+/*    OTHER DEALINGS IN THE SOFTWARE.                                   */
+/*                                                                      */
+/************************************************************************/
+
+#ifndef VIGRA2_WINDOWS_H
+#define VIGRA2_WINDOWS_H
+
+// prevent the global namespace to become polluted with
+// badly named Windows macros
+
+#if defined(_WIN32)
+# define VC_EXTRALEAN
+# ifndef NOMINMAX
+#  define NOMINMAX
+#  define _VIGRA_UNDEFINE_NOMINMAX
+# endif
+# include <windows.h>
+# ifdef _VIGRA_UNDEFINE_NOMINMAX
+#  undef NOMINMAX
+#  undef _VIGRA_UNDEFINE_NOMINMAX
+# endif
+# ifdef DIFFERENCE
+#  undef DIFFERENCE
+# endif
+# ifdef IN
+#  undef IN
+# endif
+# ifdef OUT
+#  undef OUT
+# endif
+#endif
+
+#endif /* VIGRA2_WINDOWS_H */
